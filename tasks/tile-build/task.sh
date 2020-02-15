902f40a019fe8ead45f43d9d6b4decf3139744cc
Aaron Spiegel <aaron.spiegel@emc.com>
2020-02-15 11:33:05 -0600

    Add metadata to tile-build task

--- tasks/tile-build/task.sh
+++ tasks/tile-build/task.sh
@@ -16,6 +16,9 @@ else
   $CMD build "${VERSION}"
 fi
 
-cp product/*.pivotal ../tile-product
-
+METADATA_FILE="product/metadata/${PRODUCT_NAME}.yml"
+YAML_TO_JSON=("python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)'")
+echo ${METADATA_JSON} > manifest.json
+VERSION=$(echo ${METADATA_JSON} | jq '.product_version' | sed -e 's/"//g')
+tar czfvp "../tile-product/ecs-service-broker-${VERSION}.tgz" "ecs-service-broker-${VERSION}.pivotal" manifest.json
 popd
