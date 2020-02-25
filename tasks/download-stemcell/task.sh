2cfb1d273462bbc0546c16a3ac70acf84f55608c
Aaron Spiegel <aaron.spiegel@emc.com>
2020-02-25 09:53:09 -0600

    Make dynamic stemcell lookups optional
    The “download-stemcell” task previously looked up stemcell information using product information from pivnet.  When creating a tile dynamically during development, this isn’t available, so this PR adds an option to manually specific the stemcell version and product “slug”.

--- tasks/download-stemcell/task.sh
+++ tasks/download-stemcell/task.sh
@@ -12,23 +12,24 @@ chmod +x $PIVNET_CLI
 chmod +x ./jq/jq-linux64
 JQ_CMD=./jq/jq-linux64
 
-SC_DETAILS=`cat ./pivnet-product/metadata.json | $JQ_CMD -r '[.Dependencies[] | select(.Release.Product.Name | contains("Stemcells"))][0]'`
+if [ "${STEMCELL_VERSION}" == "" ] || [ "${PRODUCT_SLUG}" == "" ]; then
+  SC_DETAILS=`cat ./pivnet-product/metadata.json | $JQ_CMD -r '[.Dependencies[] | select(.Release.Product.Name | contains("Stemcells"))][0]'`
+  STEMCELL_VERSION=$(echo "$SC_DETAILS" | $JQ_CMD -r '.Release.Version')
+  PRODUCT_SLUG=$(echo "$SC_DETAILS" | $JQ_CMD -r '.Release.Product.Slug')
+fi
 
-SC_VERSION=$(echo "$SC_DETAILS" | $JQ_CMD -r '.Release.Version')
-PRODUCT_SLUG=$(echo "$SC_DETAILS" | $JQ_CMD -r '.Release.Product.Slug')
-
-echo "Downloading stemcell $SC_VERSION"
+echo "Downloading stemcell ${STEMCELL_VERSION}"
 $PIVNET_CLI login --api-token="$PIVNET_API_TOKEN"
 
 set +e
-RESPONSE=`$PIVNET_CLI releases -p $PRODUCT_SLUG | grep $SC_VERSION`
+RESPONSE=`$PIVNET_CLI releases -p "${PRODUCT_SLUG}" | grep "${STEMCELL_VERSION}"`
 set -e
 
 if [[ -z "$RESPONSE" ]]; then
-  STEMCELL_NAME=bosh-stemcell-$SC_VERSION-$IAAS_TYPE-ubuntu-$STEMCELL_TYPE-go_agent.tgz
+  STEMCELL_NAME="bosh-stemcell-${STEMCELL_VERSION}-$IAAS_TYPE-ubuntu-${STEMCELL_TYPE}-go_agent.tgz"
   wget --show-progress https://s3.amazonaws.com/bosh-core-stemcells/$IAAS/$STEMCELL_NAME
 else
-  $PIVNET_CLI download-product-files -p $PRODUCT_SLUG -r $SC_VERSION -g "*$IAAS_TYPE*" --accept-eula
+  $PIVNET_CLI download-product-files -p "${PRODUCT_SLUG}" -r "${STEMCELL_VERSION}" -g "*${IAAS_TYPE}*" --accept-eula
 fi
 
 SC_FILE_PATH=`find ./ -name *.tgz`
